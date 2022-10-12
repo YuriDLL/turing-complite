dc5c4b510943438f8cedf98ef65ceeba3359fb3e
YuriDLL <fifnarr@gmail.com>
2022-10-12 22:51:50 +0300

    complete unseen fruit

--- src/commands.asm
+++ src/commands.asm
@@ -12,12 +12,14 @@
     SUB  {des1: source}, {val: i8}, {rez: source} => 0x41 @ des1 @ val @ rez
     SUB  {des1: source}, {des2: source}, {rez: source} => 0x41 @ des1 @ des2 @ rez
     MOV {val: i8}, {des: source} => 0xC0 @ val @ 0x00 @ des
-    MOV {des1: source}, {des2: source} => 0x00 @ des1 @ 0x00 @ des2
+    MOV {des1: source}, {des2: source} => 0x40 @ des1 @ 0x00 @ des2
     LDR {des: source}, {mem_addr: i8} => 0x7E @ mem_addr @ 0x00 @ des
     LDR {des: source}, {des_addr: source} => 0x3E @ des_addr @ 0x00 @ des
     STR {des: source}, {mem_addr: i8} => 0x7F @ mem_addr @ des @ 0x00
     STR {des: source}, {des_addr: source} => 0x3F @ des_addr @ des @ 0x00
+    GOTO {addr: i8} => 0xE0 @ 0x00 @ 0x00 @ addr
     BE {des1: source}, {des2: source}, {addr: i8} => 0x20 @ des1 @ des2 @ addr
+    BE {des1: source}, {val: i8}, {addr: i8} => 0x60 @ des1 @ val @ addr
     BNE {des1: source}, {des2: source}, {addr: i8} => 0x21 @ des1 @ des2 @ addr
     BL {des1: source}, {des2: source}, {addr: i8} => 0x22 @ des1 @ des2 @ addr
     BL {des1: source}, {val: i8}, {addr: i8} => 0x62 @ des1 @ val @ addr
--- src/unseen_fruit.asm
+++ src/unseen_fruit.asm
@@ -1,2 +1,48 @@
 #include "commands.asm"
 
+turn_left = 0x00
+forward = 0x01
+turn_rigth = 0x02
+sleep = 0x03
+use = 0x04
+shoot = 0x05
+
+coveyor = 0x5C
+;r0 - array iter
+;r1 - array len
+
+;go on position
+MOV turn_left, out
+MOV forward, out
+MOV turn_left, out
+MOV forward, out
+MOV forward, out
+MOV forward, out
+MOV forward, out
+MOV turn_left, out
+MOV forward, out
+MOV turn_rigth, out
+MOV forward, out
+
+MOV 0, r1
+loop:
+    MOV sleep, out
+    MOV inp, r2
+    BE r2, coveyor, loop
+    ;ceck that it is first fruit
+    MOV 0, r0 ; iter
+    check_loop:
+        BE r0, r1, is_first
+        LDR r3, r0
+        ADD r0, 0x01, r0
+        BNE r2, r3, check_loop
+    ;not first
+    MOV turn_rigth, out
+    MOV use, out
+    MOV turn_left, out
+    GOTO loop
+    is_first:
+        ;insert r2 to array
+        STR r2, r1
+        ADD r1, 0x01, r1
+        GOTO loop
